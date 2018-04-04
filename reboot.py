0e111abc8dc3b550438a1b95d03fdba865909c81
Justsoos <justso@gmail.com>
2018-04-04 23:21:53 +0800

    support more than one way auth

--- reboot.py
+++ reboot.py
@@ -4,11 +4,16 @@ __author__ = 'JZ'
 __homepage__ = 'https://github.com/Justsoos/reboot-tplink'
 
 import requests
+import base64
 
 def reboot(host, user, password):
 	try:
 		url = 'http://{}/userRpm/SysRebootRpm.htm?Reboot='.format(host)
 		headers = {'Referer': url}
+		
+		auth = (base64.b64encode('{}:{}'.format(user, password).encode('utf-8'))).decode('utf-8')
+		headers['Cookie'] = 'Authorization=Basic%20{}; ChgPwdSubTag='.format(auth)
+		
 		r = requests.get(url, headers=headers, auth=(user, password))
 	except:
 		raise
